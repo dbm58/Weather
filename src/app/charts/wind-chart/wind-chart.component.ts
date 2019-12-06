d457f16735d1d32bb6b0935e7cbbb821442b171e
dbm <2196445+dbm58@users.noreply.github.com>
2019-12-05 21:15:32 -0600

    styled wind chart

--- src/app/charts/wind-chart/wind-chart.component.ts
+++ src/app/charts/wind-chart/wind-chart.component.ts
@@ -30,13 +30,43 @@ export class WindChartComponent implements OnInit
       display:   true,
       text:      'Wind Speed'
     },
-    legend: { display: false }
+
+    legend: { display: false },
+
+    scales:
+    {
+      xAxes:
+      [
+        {
+          type: 'time',
+          time:
+          {
+            unit: 'hour',
+            displayFormats: { hour: 'HH' }
+          }
+        }
+      ]
+    },
+
+    tooltips :
+    {
+      callbacks:
+      {
+        label: ( tooltipItem, data ) =>
+        {
+          let wind = data.datasets[0].data[tooltipItem.index];
+          let gust = data.datasets[1].data[tooltipItem.index];
+          let label = 'Wind ' + wind + ( ( !gust || wind == gust ) ? '' : ' Gust ' + gust );
+          return label;
+        }
+      }
+    }
   };
 
   lineChartColors: Color[] =
   [
     {
-      borderColor:     'black',
+      borderColor:     'silver',
       backgroundColor: 'rgba(255,0,0,0.3)',
     },
     {
@@ -56,23 +86,29 @@ export class WindChartComponent implements OnInit
     this.data = dataService.getForecast( );
     var hourly = this.data.hourly.data.filter( (_,i) => i < 24 );
 
-    this.lineChartLabels = hourly.map( x => format( fromUnix( x.time ), 'HH') );
+    this.lineChartLabels = hourly.map( x => fromUnix( x.time ) );
 
+    var pointImage = new Image( 30, 30 );
+    pointImage.src = './assets/wi-wind-deg.svg';
+    
     this.lineChartData =
     [
       {
         data: hourly.map( x => x.windSpeed ),
         fill: false,
-        label: 'Wind Speed'
+        label: 'Wind Speed',
+        hitRadius: 20,
+        pointStyle: pointImage,
+        rotation: hourly.map( x => x.windBearing ),
       },
       {
         data: hourly.map( x => x.windGust != x.windSpeed ? x.windGust : null ),
         fill: false,
-        label: 'Wind Gust'
+        label: 'Wind Gust',
+        hitRadius: 20
       }
     ];
   }
   
-
   ngOnInit() { }
 }
