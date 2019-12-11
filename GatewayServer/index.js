db5acafa859e115b31f5a0a89eefcd340d33312d
dbm <2196445+dbm58@users.noreply.github.com>
2019-12-11 11:48:13 -0600

    Gateway Server:  added some logging; added a check to make sure that the dark sky api key was set before starting

--- GatewayServer/index.js
+++ GatewayServer/index.js
@@ -1,3 +1,9 @@
+if( ! process.env.darkSkyKey )
+{
+  console.info( '"darkSkyKey" environment variable is not set' );
+  process.exit( 1 );
+}
+
 const express = require( 'express' );
 const unirest = require( 'unirest' );
 const cors    = require( 'cors'    );
@@ -14,10 +20,13 @@ app.use( cors( ) )
    .get( '/forecast',
          ( req, res ) =>
          {
+           console.info( 'Handling /forecast request' );
+
            const lat = '43.044';
            const lon = '-88.002';
            const key = process.env.darkSkyKey;
            const url = `https://api.darksky.net/forecast/${key}/${lat},${lon}`;
+
            let request = unirest( 'GET', url );
            request.query
                    (
@@ -30,8 +39,11 @@ app.use( cors( ) )
                         response =>
                         {
                           if( response.error )
+                          {
+                            console.info( response.error );
                             res.status( 500 )
                                .end( );
+                          }
                           res.status( 200 )
                              .send( response.body );
                         }
